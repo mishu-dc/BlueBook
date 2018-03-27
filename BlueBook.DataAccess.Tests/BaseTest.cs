0bd0c3394c8bdc9f307168fcf114192cc746596c
faisal ahmed <mishu_dc@yahoo.com>
2018-03-26 23:31:05 -0400

    Data Access Unit Test Project

---
+++ BlueBook.DataAccess.Tests/BaseTest.cs
@@ -0,0 +1,21 @@
+﻿using BlueBook.DataAccess.Configurations;
+using BlueBook.Entity.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBook.DataAccess.Tests
+{
+    public class BaseTest
+    {
+        public UnitOfWork UnitOfWork { get; set; }
+
+        public BaseTest()
+        {
+            ApplicationDbContext context = new ApplicationDbContext(Effort.DbConnectionFactory.CreateTransient());
+            UnitOfWork = new UnitOfWork(context);
+        }
+    }
+}
---
+++ BlueBook.DataAccess.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BlueBook.DataAccess.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BlueBook.DataAccess.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8fd23730-9720-47ac-83b3-7610faa1e931")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
