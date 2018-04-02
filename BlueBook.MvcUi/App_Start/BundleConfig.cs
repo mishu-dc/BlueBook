568dc36744c10cbbb3f09c2c1528639c564539f7
faisal ahmed <mishu_dc@yahoo.com>
2018-04-01 22:54:29 -0400

    Installed jQuery.UI.Combined -Version 1.12.1
    Re factored BundleConfig file

--- BlueBook.MvcUi/App_Start/BundleConfig.cs
+++ BlueBook.MvcUi/App_Start/BundleConfig.cs
@@ -9,23 +9,17 @@ namespace BlueBook.MvcUi
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Assets/Scripts/jquery-{version}.js"));
+                        "~/Assets/Scripts/jQuery/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Assets/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Assets/Scripts/modernizr-*"));
+                        "~/Assets/Scripts/jQuery/jquery.validate*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Assets/Scripts/bootstrap.js",
-                      "~/Assets/Scripts/respond.js"));
+                      "~/Assets/Scripts/bootstrap/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Assets/Content/bootstrap.css",
-                      "~/Assets/Content/site.css"));
+                      "~/Assets/css/bootstrap/bootstrap-lumen.css",
+                      "~/Assets/css/site.css"));
         }
     }
 }
