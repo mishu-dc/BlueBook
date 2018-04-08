043e073b0de17ee776d06d87d57e97b4f0b8185c
faisal ahmed <mishu_dc@yahoo.com>
2018-04-08 11:40:53 -0400

    Added Log4Net in BlueBook.MvcUi Project

--- BlueBook.MvcUi/Global.asax.cs
+++ BlueBook.MvcUi/Global.asax.cs
@@ -1,17 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "Web.config", Watch = true)]
 namespace BlueBook.MvcUi
 {
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
+            log4net.Config.XmlConfigurator.Configure(new FileInfo(Server.MapPath("~/Web.config")));
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
