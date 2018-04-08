cfa40883a377d595805fdb25f0ec4a358b2ad5d9
faisal ahmed <mishu_dc@yahoo.com>
2018-04-08 11:56:04 -0400

    Added Logging in Distributor Controller

--- BlueBook.MvcUi/Controllers/DistributorController.cs
+++ BlueBook.MvcUi/Controllers/DistributorController.cs
@@ -13,15 +13,20 @@ namespace BlueBook.MvcUi.Controllers
 {
     public class DistributorController : Controller
     {
+
+        readonly log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         UnitOfWork _unitOfWork = null;
 
         public DistributorController(UnitOfWork unitOfWork)
         {
+            logger.Debug("Distributor Controller is initialized with UnitOfWork");
             _unitOfWork = unitOfWork;
         }
 
         public ActionResult Index()
         {
+            logger.Debug("Distributor Controller Index action method is invoked");
             return View();
         }
 
@@ -34,10 +39,14 @@ namespace BlueBook.MvcUi.Controllers
                 List<Distributor> records = await model.GetDistributorsByPageAsync(page, limit, sortBy, direction, code, name);
                 int total = await model.GetTotalDistributorsAsync(page, limit, sortBy, direction, code, name);
 
+                logger.Debug("Distributor Controller List action method is invoked");
+                logger.Debug(string.Format("Total {0} distributors are found and {1} is returned", total, records.Count()));
+
                 return this.Json(new { records, total }, JsonRequestBehavior.AllowGet);
             }
             catch(Exception ex)
             {
+                logger.Error("Error while invoking List action method: " , ex);
                 return this.Json(ex.Message, JsonRequestBehavior.AllowGet);
             }
         }
@@ -51,6 +60,7 @@ namespace BlueBook.MvcUi.Controllers
                 if (ModelState.IsValid)
                 {
                     await model.SaveAsync(record);
+                    logger.Debug("Distributor saved successfully.");
                     return Json(new { result = true }, JsonRequestBehavior.AllowGet);
                 }
                 else
@@ -60,6 +70,7 @@ namespace BlueBook.MvcUi.Controllers
             }
             catch(Exception ex)
             {
+                logger.Error("Error while invoking Save action method: ", ex);
                 return Json(new { result = false, ex.Message }, JsonRequestBehavior.AllowGet);
             }
         }
@@ -71,10 +82,12 @@ namespace BlueBook.MvcUi.Controllers
             {
                 DistributorModel model = new DistributorModel(_unitOfWork);
                 await model.DeleteAsync(id);
+                logger.Debug("Distributor deleted successfully.");
                 return Json(new { result = true }, JsonRequestBehavior.AllowGet);
             }
             catch (Exception ex)
             {
+                logger.Error("Error while invoking Delete action method: ", ex);
                 return Json(new { result = false, ex.Message }, JsonRequestBehavior.AllowGet);
             }
         }
