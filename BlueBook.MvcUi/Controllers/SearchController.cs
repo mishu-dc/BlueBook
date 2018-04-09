50803d4a1b090747b57ec1d3de2e269fd89996a7
faisal ahmed <mishu_dc@yahoo.com>
2018-04-08 22:35:12 -0400

    Added Search Controller to populate Brands

---
+++ BlueBook.MvcUi/Controllers/SearchController.cs
@@ -0,0 +1,45 @@
+﻿using BlueBook.DataAccess.Entities;
+using BlueBook.Entity.Configurations;
+using BlueBook.MvcUi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlueBook.MvcUi.Controllers
+{
+    public class SearchController : Controller
+    {
+        readonly log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        UnitOfWork _unitOfWork = null;
+
+        //Initialize UnitOfWork using Ninject
+        public SearchController(UnitOfWork unitOfWork)
+        {
+            logger.Debug("Search Controller is initialized with UnitOfWork");
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<JsonResult> GetBrands()
+        {
+            try
+            {
+                logger.Debug("Brand Controller List action method is invoked");
+
+                BrandModel model = new BrandModel(_unitOfWork);
+
+                List<Brand> records = await model.GetBrandsByPageAsync(null, null, string.Empty, string.Empty, string.Empty, string.Empty);
+                
+                return this.Json(records.Select(x=> new { Id = x.Id, Name = x.Name }) , JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                logger.Error("Error while invoking List action method: ", ex);
+                return this.Json(new { error = ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
