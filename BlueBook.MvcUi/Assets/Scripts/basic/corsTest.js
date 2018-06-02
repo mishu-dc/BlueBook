a194dca162b2c7c31da8dbcd871a8c8381f44ffb
faisal ahmed <mishu_dc@yahoo.com>
2018-06-02 15:46:04 -0400

    Added controller and view to test Cors.

---
+++ BlueBook.MvcUi/Assets/Scripts/basic/corsTest.js
@@ -0,0 +1,18 @@
+﻿
+$(document).ready(function(){
+    var serviceUrl = 'http://localhost:59821/api/brands';
+
+    $("#submit").click(function(){
+        var method = $('#method').val();
+        $("#ul").empty();
+
+        $.ajax({
+            type: method,
+            url: serviceUrl
+        }).done(function (data) {
+            $.each(data, function (key, value) {
+                $("#ul").append('<li>' + value.Name + '</li>');
+            });
+        });
+    }); 
+});
---
+++ BlueBook.MvcUi/Controllers/CorsTestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlueBook.MvcUi.Controllers
+{
+    public class CorsTestController : Controller
+    {
+        // GET: CorsTest
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
