33bb5f5f6b487967c58aff604e7b449a76937d26
faisal ahmed <mishu_dc@yahoo.com>
2019-01-19 09:35:17 -0500

    Changed favicon and page title

--- BlueBook.WebApi/App_Start/SwaggerConfig.cs
+++ BlueBook.WebApi/App_Start/SwaggerConfig.cs
@@ -182,7 +182,7 @@ namespace BlueBook.WebApi
                         // Use the "DocumentTitle" option to change the Document title.
                         // Very helpful when you have multiple Swagger pages open, to tell them apart.
                         //
-                        c.DocumentTitle("Blue Book Web API Swagger");
+                        c.DocumentTitle("Asp.net Web Api Swagger");
 
                         // Use the "InjectStylesheet" option to enrich the UI with one or more additional CSS stylesheets.
                         // The file must be included in your project as an "Embedded Resource", and then the resource's
